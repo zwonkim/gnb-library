eb18a1b4ad9e7e10b88d2b1c1a92b60d59937d3d
jiwonkimm <jw.kim@muzlive.com>
2024-03-11 15:03:57 +0900

    modify gnb

---
+++ src/GNB/GNB.jsx
@@ -0,0 +1,7 @@
+import "./GNB.css";
+
+const GNB = () => {
+  return <div wrapper>gnb</div>;
+};
+
+export default GNB;
--- src/GNB/index.js
+++ src/GNB/index.js
@@ -1,7 +1,1 @@
-import "./GNB.css";
-
-const index = () => {
-  return <div wrapper>gnb</div>;
-};
-
-export default index;
+export * from "./GNB";
